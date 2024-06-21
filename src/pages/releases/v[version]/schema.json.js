ba763d095a85e5699c5179dc4543b3bd6edc93f4
Tim Schaub <tim.schaub@gmail.com>
2024-06-21 15:58:41 -0600

    Handle build metadata in release tags

--- src/pages/releases/v[version]/schema.json.js
+++ src/pages/releases/v[version]/schema.json.js
@@ -1,12 +1,12 @@
 import {releases} from '../../../releases.js';
 
 export async function getStaticPaths() {
-  return (await releases).map(r => ({params: {tag: r.tag}}));
+  return (await releases).map(r => ({params: {version: r.version}}));
 }
 
 export async function GET({params}) {
-  const tag = params.tag;
-  const release = (await releases).find(r => r.tag === tag);
+  const version = params.version;
+  const release = (await releases).find(r => r.version === version);
   if (!release) {
     return new Response('Not found', {status: 404});
   }
